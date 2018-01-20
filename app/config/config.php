64666c2a7b906b68d0cabfc06da0195066079c01
c5h11oh <tony20715@gmail.com>
2018-01-20 03:40:34 +0100

    26_initial setup

--- app/config/config.php
+++ app/config/config.php
@@ -1,16 +1,16 @@
 <?php
   //DB params
   define('DB_HOST', 'localhost');
-  define('DB_USER', '_YOUR_USER_');
-  define('DB_PASS', '_YOUR_PASS_');
-  define('DB_NAME', '_YOUR_DB_NAME_');
+  define('DB_USER', 'root');
+  define('DB_PASS', '123456');
+  define('DB_NAME', 'shareposts');
 
 
   //Define APPROOT
   define('APPROOT', dirname(dirname(__FILE__)));
   
   //Define URLROOT
-  define('URLROOT', '_YOUR_URL_');
+  define('URLROOT', 'http://localhost/shareposts');
 
   //Define SITENAME
-  define('SITENAME', '_YOUR_SITENAME_');
+  define('SITENAME', 'The SharePosts');
