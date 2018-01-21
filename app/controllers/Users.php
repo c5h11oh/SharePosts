bf46bc3d4ae7c0d9554e4522e46bc8631eeae78c
c5h11oh <tony20715@gmail.com>
2018-01-21 12:05:57 +0100

    28_Created User Controller

---
+++ app/controllers/Users.php
@@ -0,0 +1,29 @@
+<?php
+    class Users extends Controller{
+        public function __construct(){
+
+        }
+
+        public function register(){
+            //Check for posts
+            if($_SERVER['REQUEST_METHOD'] == 'POST'){
+                //Process form
+            }else{
+                //Init data
+                $data = [
+                    'name' => '',
+                    'email' => '',
+                    'password' => '',
+                    'confirm_password' => '',
+                    'name_err' => '',
+                    'email_err' => '',
+                    'password_err' => '',
+                    'confirm_password_err' => ''
+                ];
+
+                //Load view
+                $this->view('users/register', $data);
+            }
+        }
+    }
+?>
---
+++ app/views/users/register.php
@@ -0,0 +1,9 @@
+<?php include_once APPROOT . '/views/inc/header.php' ?>
+    <div class="row">
+        <div class="col-md-6 mx-auto">
+            <div class="card card-body bg-light mt-5">
+                <h2>Create an Account</h2>
+            </div>
+        </div>
+    </div>
+<?php include_once APPROOT . '/views/inc/footer.php' ?>
