93348c122121e46025250a9e114ccb9de3cc0219
c5h11oh <tony20715@gmail.com>
2018-01-29 18:33:09 +0100

    33_增加註冊後跳轉會出現flash message，告訴你OKOK，註冊成功可以登入ㄌ

--- app/bootstrap.php
+++ app/bootstrap.php
@@ -3,6 +3,7 @@
     require_once 'config/config.php';
     //Load helpers
     require_once 'helpers/url_helper.php';
+    require_once 'helpers/session_helper.php';
     
     //Load Libraries (No need anymore because the autoload below)
     // require_once 'libraries/Controller.php';
