745d35fb7a7dd1a817cb5c43707f3e260696c55c
c5h11oh <tony20715@gmail.com>
2018-01-30 02:26:42 +0100

    34_完成login的製作，包括user model去檢查帳號密碼是否正確的function，和登入成功或失敗時controller要怎麼行動並傳送錯誤到view.

--- app/controllers/Users.php
+++ app/controllers/Users.php
@@ -113,10 +113,25 @@
                     $data['password_err'] = 'Password must be at least 6 characters';
                 }
 
+                //Check for user/email
+                if($this->userModel->findUserByEmail($data['email'])){
+                    //Validated
+                }else{
+                    $data['email_err'] = 'No user found';
+                }
+
                 //Make sure errors are empty
                 if(empty($data['email_err']) && empty($data['password_err'])){
                     //Validated
-                    die('SUCCESS');
+                    //Check and set logged in user
+                    $loggedInUser = $this->userModel->login($data['email'], $data['password']);
+                    if($loggedInUser){
+                        die('SUCCESS to login');
+                        //Create Session
+                    }else{
+                        $data['password_err'] = 'Password incorrect';
+                        $this->view('users/login', $data);
+                    }
                 } else {
                     //Load view with errors
                     $this->view('users/login', $data);
--- app/models/User.php
+++ app/models/User.php
@@ -19,7 +19,20 @@
             return false;
          }
       }
+      //Login user
+      public function login($email, $password){
+         $this->db->query('SELECT * FROM users WHERE email = :email');
+         $this->db->bind(':email', $email);
 
+         $row = $this->db->single();
+
+         $hashed_password = $row->password;
+         if(password_verify($password, $hashed_password)){
+            return $row;
+         }else{
+            return false;
+         }
+      }
 
       //Find user by email
       public function findUserByEmail($email){
